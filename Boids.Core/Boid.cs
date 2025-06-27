4ce4886f64755b859153dca8b0eabf76faa7be1d
SebaRenner <sebastian.renner@gmx.ch>
2025-06-27 23:52:13 +0200

    Initialized solution

---
+++ Boids.Core/Boid.cs
@@ -0,0 +1,28 @@
+﻿using Boids.Math;
+
+namespace Boids.Core
+{
+    public class Boid
+    {
+        public Vec2 Position { get; set; }
+
+        public Vec2 Velocity { get; set; }
+
+        public Vec2 Separation(IEnumerable<Boid> neighbors)
+        {
+            var steering = new Vec2(0, 0);
+
+            foreach (var neighbor in neighbors)
+            {
+                var distance = Position.Distance(neighbor.Position);
+                var diff = Position.Subtract(neighbor.Position);
+
+                steering = new Vec2(
+                    steering.X + diff.Divide(distance * distance).X,
+                    steering.Y + diff.Divide(distance * distance).Y);
+            }
+
+            return steering;
+        }
+    }
+}
---
+++ Boids.Math/Vec2.cs
@@ -0,0 +1,33 @@
+﻿namespace Boids.Math
+{
+    public struct Vec2
+    {
+        public Vec2(double x, double y)
+        {
+            X = x; 
+            Y = y;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public Vec2 Subtract(Vec2 other)
+        {
+            return new Vec2(X - other.X, Y - other.Y);
+        }
+
+        public Vec2 Divide(double scalar)
+        {
+            return new Vec2(X / scalar, Y / scalar);
+        }
+
+        public double Distance(Vec2 other) 
+        { 
+            var dx = X - other.X;
+            var dy = Y - other.Y;
+
+            return System.Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
