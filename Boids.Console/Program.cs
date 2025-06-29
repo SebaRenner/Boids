b9c457020ed03befdabfdfc746d183d6f3d8dd20
SebaRenner <sebastian.renner@gmx.ch>
2025-06-29 17:30:23 +0200

    Added console program and On Load Lifecycle Hook to GameWIndow

---
+++ Boids.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Boids.Graphics;
+
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+var gws = GameWindowSettings.Default;
+var nws = new NativeWindowSettings()
+{
+    ClientSize = new Vector2i(800, 600),
+    Title = "Boid Simulation"
+};
+
+using var window = new BoidWindow(gws, nws);
+window.Run();
+
--- Boids.Graphics/BoidWindow.cs
+++ Boids.Graphics/BoidWindow.cs
@@ -1,6 +1,7 @@
 ﻿using Boids.Core;
 using Boids.Math;
 
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
@@ -14,16 +15,22 @@ namespace Boids.Graphics
         private const int WindowWidth = 800;
         private const int WindowHeight = 600;
 
-        public BoidWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) 
+        public BoidWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
             : base(gameWindowSettings, nativeWindowSettings)
         {
             boids = new List<Boid>();
-            for (int i = 0; i < 3; i++) 
+            for (int i = 0; i < 3; i++)
             {
                 var position = new Vec2(rng.Next(WindowWidth), rng.Next(WindowHeight));
                 var velocity = new Vec2(rng.NextDouble() * 2 - 1, rng.NextDouble() * 2 - 1);
                 boids.Add(new Boid(position, velocity));
             }
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0f, 0f, 0f, 1f);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
