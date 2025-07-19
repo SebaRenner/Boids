cc36d0eafd5fd64b1f1f1d0519157b4e03ea6034
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2025-07-19 15:18:43 +0200

    Added math library unit tests

---
+++ Boids.Math.Tests/Vec2ExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Boids.Math.Tests;
+
+public class Vec2ExtensionsTest
+{
+    [Fact]
+    public void Vec2_Sum()
+    {
+        // Arrange
+        var vec1 = new Vec2(2, 2);
+        var vec2 = new Vec2(2, 3);
+        var vec3 = new Vec2(2, 4);
+        var expected = new Vec2(6, 9);
+
+        // Act
+        var testee = new List<Vec2> { vec1, vec2, vec3 }.Sum();
+
+        // Assert
+        Assert.Equal(expected, testee);
+    }
+
+}
---
+++ Boids.Math.Tests/Vec2Test.cs
@@ -0,0 +1,137 @@
+namespace Boids.Math.Tests;
+
+public class Vec2Test
+{
+    [Fact]
+    public void Vec2_Default_Ctor()
+    {
+        // Arrange & Act
+        var testee = new Vec2();
+
+        // Assert
+        Assert.Equal(0, testee.X);
+        Assert.Equal(0, testee.Y);
+    }
+
+    [Fact]
+    public void Vec2_Ctor()
+    {
+        // Arrange
+        var x = 3.66;
+        var y = 5.913;
+
+        // Act
+        var testee = new Vec2(x, y);
+
+        // Assert
+        Assert.Equal(x, testee.X);
+        Assert.Equal(y, testee.Y);
+    }
+
+    [Fact]
+    public void Vec2_Plus()
+    {
+        // Arrange
+        var x1 = 21;
+        var y1 = 23;
+        var x2 = 4;
+        var y2 = 4;
+        var xr = x1 + x2;
+        var yr = y1 + y2;
+
+        var vec1 = new Vec2(x1, y1);
+        var vec2 = new Vec2(x2, y2);
+
+        // Act
+        var testee = vec1 + vec2;
+
+        // Assert
+        Assert.Equal(xr, testee.X);
+        Assert.Equal(yr, testee.Y);
+    }
+
+    [Fact]
+    public void Vec2_Minus()
+    {
+        // Arrange
+        var x1 = 21;
+        var y1 = 23;
+        var x2 = 4;
+        var y2 = 4;
+        var xr = x1 - x2;
+        var yr = y1 - y2;
+
+        var vec1 = new Vec2(x1, y1);
+        var vec2 = new Vec2(x2, y2);
+
+        // Act
+        var testee = vec1 - vec2;
+
+        // Assert
+        Assert.Equal(xr, testee.X);
+        Assert.Equal(yr, testee.Y);
+    }
+
+    [Fact]
+    public void Vec2_Multiply_By_Scalar()
+    {
+        // Arrange
+        var x = 21.0;
+        var y = 23.0;
+        var scalar = 3;
+        var xr = x * scalar;
+        var yr = y * scalar;
+        
+        var vec = new Vec2(x, y);
+
+        // Act
+        var testee = vec * scalar;
+
+        // Assert
+        Assert.Equal(xr, testee.X);
+        Assert.Equal(yr, testee.Y);
+    }
+
+    [Fact]
+    public void Vec2_Divide_By_Scalar()
+    {
+        // Arrange
+        var x = 21.0;
+        var y = 23.0;
+        var scalar = 3;
+        var xr = x / scalar;
+        var yr = y / scalar;
+
+        var vec = new Vec2(x, y);
+
+        // Act
+        var testee = vec / scalar;
+
+        // Assert
+        Assert.Equal(xr, testee.X);
+        Assert.Equal(yr, testee.Y);
+    }
+
+    [Fact]
+    public void Vec2_Distance()
+    {
+        // Arrange
+        var x1 = 21;
+        var y1 = 23;
+        var x2 = 4;
+        var y2 = 4;
+        var dx = x1 - x2;
+        var dy = y1 - y2;
+
+        var expected = System.Math.Sqrt(dx * dx + dy * dy);
+
+        var vec1 = new Vec2(x1, y1);
+        var vec2 = new Vec2(x2, y2);
+
+        // Act
+        var testee = vec1.Distance(vec2);
+
+        // Assert
+        Assert.Equal(expected, testee);
+    }
+}
