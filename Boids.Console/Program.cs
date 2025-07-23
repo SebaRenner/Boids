3dd97096ff3d818cd98eae0f4064a922d2343709
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2025-07-23 23:23:10 +0200

    Use Raylib for visualization

--- Boids.Console/Program.cs
+++ Boids.Console/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Boids.Graphics;
 
-var window = new BoidWindow();
+var window = new BoidWindow(1920, 1080, 2, 1);
 
--- Boids.Graphics/BoidWindow.cs
+++ Boids.Graphics/BoidWindow.cs
@@ -1,33 +1,79 @@
 ﻿using Boids.Core;
 using Boids.Math;
 
+using Raylib_cs;
+
 namespace Boids.Graphics;
 
 public class BoidWindow
 {
-    private List<Boid> boids;
+    private List<Boid> boids = new List<Boid>();
     private Random rng = new();
 
-    private const int WindowWidth = 800;
-    private const int WindowHeight = 600;
+    private int WindowWidth;
+    private int WindowHeight;
 
-    public BoidWindow()
+    public BoidWindow(int windowWidth, int windowHeight, int birds, int fps = 15)
     {
-        boids = new List<Boid>();
-        for (int i = 0; i < 3; i++)
+        WindowWidth = windowWidth;
+        WindowHeight = windowHeight;
+
+        for (int i = 0; i < birds; i++)
         {
             var position = new Vec2(rng.Next(WindowWidth), rng.Next(WindowHeight));
             var velocity = new Vec2(rng.NextDouble() * 2 - 1, rng.NextDouble() * 2 - 1);
             boids.Add(new Boid(position, velocity));
         }
+
+        Raylib.InitWindow(WindowWidth, WindowHeight, "Boids");
+
+        Raylib.SetTargetFPS(fps);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            UpdateFrame();
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            DrawBoids();
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
     }
 
-    public void UpdateFrame()
+    private void UpdateFrame()
     {
         foreach (var boid in boids)
         {
             var neighbors = boids.Where(b => b != boid);
             boid.Update(neighbors);
+            WrapBoid(boid);
         }
+    }
+
+    private void DrawBoids()
+    {
+        foreach (var boid in boids)
+        {
+            Raylib.DrawCircle((int)boid.Position.X, (int)boid.Position.Y, 15, Color.Black);
+        }
+    }
+
+    private void WrapBoid(Boid boid)
+    {
+        var pos = boid.Position;
+
+        var x = pos.X;
+        var y = pos.Y;
+
+        if (x < 0) x += WindowWidth;
+        if (x >= WindowWidth) x -= WindowWidth;
+        if (y < 0) y += WindowHeight;
+        if (y >= WindowHeight) y -= WindowHeight;
+
+        boid.Position = new Vec2(x, y);
     }
 }
